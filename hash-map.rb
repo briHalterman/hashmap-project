702a146683621e915a25255651fad17ee1cee478
Brittany Halterman <halterma@gmail.com>
2025-05-07 11:36:39 -0400

    Define hash method

--- hash-map.rb
+++ hash-map.rb
@@ -17,16 +17,4 @@ class HashMap
     hash_code
   end
 
-  def set(key, value)
-    # Take the key, hash it, and convert it into a valid index for the buckets array
-    hash_code = hash(key)
-    index = hash_code % @capacity
-
-    # Check that the index is valid
-    raise IndexError if index.negative? || index >= @buckets.length
-
-    # Grab bucket at index
-    # If there's nothing there, start it as an empty array
-    bucket = @buckets[index] ||= []
-  end
 end
