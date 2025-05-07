f2c0e20b5e854b185caedca14c4ff7c89b10167d
Brittany Halterman <halterma@gmail.com>
2025-05-07 11:46:45 -0400

    Implement set method for storing key-value pairs
    - Hashes string keys
    - Calculates a valid index
    - Initializes buckets
    - Handles collisions
    - Overwrites value if key already exists in the buckets
    - Adds new key-value pair and updates size

--- hash-map.rb
+++ hash-map.rb
@@ -17,4 +17,28 @@ class HashMap
     hash_code
   end
 
+  def set(key, value)
+    # Take the key, hash it, and convert it into a valid index for the buckets array
+    hash_code = hash(key)
+    index = hash_code % @capacity
+
+    # Check that the index is valid
+    raise IndexError if index.negative? || index >= @buckets.length
+
+    # Grab bucket at index
+    # If there's nothing there, start it as an empty array
+    bucket = @buckets[index] ||= []
+
+    # Check if the key already exists in the bucket
+    bucket.each do |pair|
+      if pair[0] == key
+        pair[1] = value # Overwrite the value
+        return          # Exit early after update
+      end
+    end
+
+    # If key was not found, add a new pair
+    bucket << [key, value]
+    @size += 1
+  end
 end
